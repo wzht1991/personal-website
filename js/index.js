ce65b1d2602267456711e8379def6df427f6e956
wzht1991 <506486189@qq.com>
2019-01-10 16:57:22 +1000

    main page-finish

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ list=document.querySelectorAll(".list");
 // 记录使用过的那个nav
 var used=null;
 for(let i=0;i<list.length;i++){
-    list[i].addEventListener("click", function(){
+    list[i].addEventListener("click", ()=>{
          list[i].style.color="#191970";
          list[i].style.borderBottom="3px solid #191970";
         // 如果之前点过 就把之前点过的nav 变成原样 不然就会出现多个nav 有下滑线 和字体是蓝色
